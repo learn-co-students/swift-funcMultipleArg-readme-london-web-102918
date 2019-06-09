53e8fea0a255ab03901ddf8aeefe3c66b5673756
github username <github email address>
2019-06-09 19:11:04 +0100

    Functions with multiple arguments added

--- Moons.playground/Contents.swift
+++ Moons.playground/Contents.swift
@@ -4,40 +4,93 @@ func moonsOfJupiter() {
 
 moonsOfJupiter()
 
-
-// planetDescription, take 1
-// The bit of code below is commented out, since we iterate on this definition of planetDescription() below.
-/*
 func planetDescription(planet: String) {
-    print("There are 67 moons orbiting \(planet).")
+    print("There are 67 moons orbiting \(planet)")
 }
-
-planetDescription(planet: "Jupiter")
 
 var planet = "Mars"
 
 planetDescription(planet: planet)
-*/
 
-
-func greet(name: String, greeting: String) {
-    print("\(greeting), \(name)!")
+func greet (name: String, greeting: String) {
+    print("\(greeting), \(name) ")
 }
-
 
 func planetDescription(planet: String, numberOfMoons: Int) {
     print("There are \(numberOfMoons) moons orbiting \(planet)")
 }
 
-// This line of code gives an error.
-// Uncomment it to see what the error is!
-//planetDescription("Jupiter", 67)
-
-planetDescription(planet: "Jupiter", numberOfMoons: 67)
-
-var planet = "Jupiter"
+planet = "Jupiter"
 var moons = 67
 planetDescription(planet: planet, numberOfMoons: moons)
+
 planet = "Mars"
 moons = 2
 planetDescription(planet: planet, numberOfMoons: moons)
+
+func theBestFunction(firstParameterName: String, secondParameterName: String) {
+    print("\(firstParameterName) \(secondParameterName)")
+}
+
+theBestFunction(firstParameterName:secondParameterName)
+
+func theBestFunction(
+    _ firstParameterName: String,
+    _ secondParameterName: String,
+    _ thirdParameterName: String,
+    _ fourthParameterName: String) {
+    
+    print(firstParameterName)
+    print(secondParameterName)
+    print(thirdParameterName)
+    print(fourthParameterName)
+    
+}
+
+
+
+
+//func moonsOfJupiter() {
+//    print("There are 67 moons orbiting Jupiter.")
+//}
+//
+//moonsOfJupiter()
+//
+//
+//// planetDescription, take 1
+//// The bit of code below is commented out, since we iterate on this definition of planetDescription() below.
+///*
+//func planetDescription(planet: String) {
+//    print("There are 67 moons orbiting \(planet).")
+//}
+//
+//planetDescription(planet: "Jupiter")
+//
+//var planet = "Mars"
+//
+//planetDescription(planet: planet)
+//*/
+//
+//
+//func greet(name: String, greeting: String) {
+//    print("\(greeting), \(name)!")
+//}
+//
+//
+//func planetDescription(planet: String, numberOfMoons: Int) {
+//    print("There are \(numberOfMoons) moons orbiting \(planet)")
+//}
+//
+//// This line of code gives an error.
+//// Uncomment it to see what the error is!
+////planetDescription("Jupiter", 67)
+//
+//planetDescription(planet: "Jupiter", numberOfMoons: 67)
+//
+//var planet = "Jupiter"
+//var moons = 67
+//planetDescription(planet: planet, numberOfMoons: moons)
+//planet = "Mars"
+//moons = 2
+//planetDescription(planet: planet, numberOfMoons: moons)
+
